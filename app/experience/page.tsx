55fa6cfb905c2e19132c05d524e8e1742677c12f
Theviyanthan Krishnamohan <theviyanthan@outlook.com>
2025-12-16 23:10:37 +0530

    feat(data): add work and educational experience

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -29,32 +29,40 @@ const ExperiencePage: FC = (): ReactElement => {
                 {experienceData.description}
             </Typography>
             <Stepper orientation="vertical">
-                {experienceData.items?.map((edu, index) => (
+                {experienceData.items?.map((exp, index) => (
                     <Box key={index}>
                         <Image
                             className="institute-image"
-                            src={edu.image}
-                            alt={edu.institution}
+                            src={exp.image}
+                            alt={exp.institution}
                             width={40}
                             height={40}
                         />
                         <Typography variant="h6" color="var(--portfolio-palette-primary-contrastText)">
-                            {edu.position}
+                            {exp.position}
                         </Typography>
                         <Typography variant="subtitle1" color="var(--portfolio-palette-primary-contrastText)">
-                            {edu.institution}
+                            {exp.institution}
                         </Typography>
                         <Typography variant="subtitle2" color="var(--portfolio-palette-primary-contrastText)">
-                            {edu.duration}
+                            {exp.duration}
                         </Typography>
                         <Typography variant="body1" color="var(--portfolio-palette-primary-contrastText)">
-                            {edu.description}
+                            {Array.isArray(exp.description) ? (
+                                <ul>
+                                    {exp.description.map((point, pointIndex) => (
+                                        <li key={pointIndex}>{point}</li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                exp.description
+                            )}
                         </Typography>
                         <Typography variant="h6" color="var(--portfolio-palette-primary-contrastText)">
                             Awards & Scholarships
                         </Typography>
-                        <List sx={{display: "flex", flexDirection: "row", gap: "10px", flexWrap: "wrap"}}>
-                            {edu.awards?.map((award, awardIndex) => (
+                        <List sx={{ display: "flex", flexDirection: "row", gap: "10px", flexWrap: "wrap" }}>
+                            {exp.awards?.map((award, awardIndex) => (
                                 <ListItem key={awardIndex}>
                                     <ListItemAvatar>
                                         <MilitaryTechIcon color="action" />
@@ -70,36 +78,39 @@ const ExperiencePage: FC = (): ReactElement => {
                         <Typography variant="h6" color="var(--portfolio-palette-primary-contrastText)">
                             Projects
                         </Typography>
-                        <Box sx={{display: "flex", flexDirection: "row", gap: "20px", flexWrap: "wrap"}}>
-                            {edu.projects?.map((projectSlug, projectIndex) => {
+                        <Box sx={{ display: "flex", flexDirection: "row", gap: "20px", flexWrap: "wrap" }}>
+                            {exp.projects?.map((projectSlug, projectIndex) => {
                                 const project = portfolioData.projects.items?.find((p) => p.slug === projectSlug);
                                 if (!project) return null;
 
                                 return (
-                                    <Card key={ projectIndex } sx={ { marginBottom: "10px", maxWidth: "400px" } }>
-                                        <Link href={`projects/${project.slug}`} style={{ textDecoration: "none", color: "inherit" }}>
-                                        <CardActionArea sx={{ display: "flex" }}>
-                                            <CardMedia
-                                                component={Image}
-                                                image={project.image}
-                                                alt={project.title}
-                                                width={60}
-                                                height={30}
-                                            />
-                                            <Box sx={{ padding: "10px" }}>
-                                                <Typography
-                                                    variant="h6"
-                                                    color="var(--portfolio-palette-primary-contrastText)"
-                                                >
-                                                    {project.title}
-                                                </Typography>
-                                                <Typography
-                                                    variant="body2"
-                                                    color="var(--portfolio-palette-primary-contrastText)"
-                                                >
-                                                    {project.description}
-                                                </Typography>
-                                            </Box>
+                                    <Card key={projectIndex} sx={{ marginBottom: "10px", maxWidth: "400px" }}>
+                                        <Link
+                                            href={`projects/${project.slug}`}
+                                            style={{ textDecoration: "none", color: "inherit" }}
+                                        >
+                                            <CardActionArea sx={{ display: "flex" }}>
+                                                <CardMedia
+                                                    component={Image}
+                                                    image={project.image}
+                                                    alt={project.title}
+                                                    width={60}
+                                                    height={30}
+                                                />
+                                                <Box sx={{ padding: "10px" }}>
+                                                    <Typography
+                                                        variant="h6"
+                                                        color="var(--portfolio-palette-primary-contrastText)"
+                                                    >
+                                                        {project.title}
+                                                    </Typography>
+                                                    <Typography
+                                                        variant="body2"
+                                                        color="var(--portfolio-palette-primary-contrastText)"
+                                                    >
+                                                        {project.description}
+                                                    </Typography>
+                                                </Box>
                                             </CardActionArea>
                                         </Link>
                                     </Card>
@@ -107,7 +118,7 @@ const ExperiencePage: FC = (): ReactElement => {
                             })}
                         </Box>
                         <Box>
-                            {edu.skills?.map((skill, skillIndex) => (
+                            {exp.skills?.map((skill, skillIndex) => (
                                 <Chip
                                     key={skillIndex}
                                     label={skill}
