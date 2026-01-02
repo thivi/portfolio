f883776b575886cf1cdda279b2b3e91e9411a36a
Theviyanthan Krishnamohan <theviyanthan@outlook.com>
2026-01-02 13:17:24 +0530

    feat(app): add robots file

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,16 @@
 import type { MetadataRoute } from "next";
 import { loadPortfolioData } from "../data/portfolio";
 import { Portfolio } from "../models/portfolio";
+import { BASE_URL } from "../constants/path";
 
 const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
     const data: Portfolio = await loadPortfolioData();
     const sitemapEntries: MetadataRoute.Sitemap = [];
-    const baseUrl: string = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     Object.entries(data).forEach(([ _, section ]: [ string, typeof data[ keyof Portfolio ] ]) => {
         const entry: MetadataRoute.Sitemap[ number ] = {
-            url: `${baseUrl}/${ section.slug }`,
+            url: `${BASE_URL}/${ section.slug }`,
             lastModified: new Date().toISOString(),
             changeFrequency: "monthly",
         };
@@ -21,7 +21,7 @@ const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
             section.items?.forEach((item: typeof section.items[ number ]) => {
                 if ("slug" in item) {
                     const entry: MetadataRoute.Sitemap[ number ] = {
-                        url: `${baseUrl}/${ section.slug }/${ item.slug }`,
+                        url: `${BASE_URL}/${ section.slug }/${ item.slug }`,
                         lastModified: new Date().toISOString(),
                         changeFrequency: "monthly",
                         ...("image" in section && {
